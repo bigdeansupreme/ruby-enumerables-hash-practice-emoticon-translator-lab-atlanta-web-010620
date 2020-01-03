0f51827c6ce879d48c2a87a7e2b19fde7f613324
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 13:05:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,11 @@ def load_library(file_path)
   file = File.read(file_path)
     emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
      YAML.load_file(file_path).each do |meaning, describe|
+       eng, jan = describe
+     emoticons["get_meaning"][jan] = meaning
+     emoticons["get_emoticon"][eng] = jan
+  end
+  emoticons
 end
 
 def get_japanese_emoticon(file_path, emoticon)
