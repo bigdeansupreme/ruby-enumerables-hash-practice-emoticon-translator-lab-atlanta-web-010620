934bde032e8f1d3c95bb69f52065d837648c414a
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 13:10:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,6 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  
+  emoticons = load_library(file_path)
+  result = emoticons["get_meaning"][emoticon]
 end
